c89e46fe2dc7062253a630c4662ef46fef0260bc
Brett Gamble <brettkgamble@gmail.com>
2020-01-12 12:03:20 -0700

    036 - next.config.js added publicRunTimeConfig

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const optimizedImages = require('next-optimized-images');
 
 const cssConfig = {cssModules: true}
 const nextConfig = {
+  publicRuntimeConfig: {
+    base_url: BASE_URL
+  },
   webpack(config)  {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
     return config
@@ -17,46 +20,3 @@ module.exports = withPlugins([
     [optimizedImages, {}],
   [withCSS, cssConfig]
 ], nextConfig);
-
-
-
-
-
-// require("dotenv").config();
-//
-// const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images')
-//
-// const path = require("path");
-// const Dotenv = require("dotenv-webpack");
-//
-// module.exports = {
-//   webpack: config => {
-//     config.plugins = config.plugins || [];
-//
-//     config.plugins = [
-//       ...config.plugins,
-//
-//       // Read the .env file
-//       new Dotenv({
-//         path: path.join(__dirname, ".env"),
-//         systemvars: true
-//       })
-//     ];
-//
-//     return config;
-//   }
-// };
-//
-// const withCSS = require('@zeit/next-css')
-// module.exports = withCSS({
-//     cssModules:true
-// });
-//
-//
-// const withSASS = require('@zeit/next-sass');
-// const withOptimizedImages = require('next-optimized-images');
-// module.exports = withOptimizedImages(withCSS(withSASS ({
-//     cssModules:true
-// })));
-
