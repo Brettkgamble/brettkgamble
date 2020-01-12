044fb60ee647a2675f7a548dbaa762a5fab6ac27
Brett Gamble <brettkgamble@gmail.com>
2020-01-12 11:50:55 -0700

    035 - server.js configured for robots.txt

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const robotsOptions = {
+    root: __dirname + '/static/',
+    headers: {
+        'Content-Type': 'charset-UTF-8'
+    }
+}
+
 app.prepare()
     .then(() => {
         const server = express();
+
+        server.get('/robots.txt', (req, res) => {
+            return res.status(200).sendFile('robots.txt', robotsOptions)
+        });
 
         // handle everything else
         server.get('*',(req, res)=>{
